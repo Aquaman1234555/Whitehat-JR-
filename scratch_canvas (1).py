8caa2038c2a2aa3e14196515757be4406841331b
Aquaman1234555 <101315879+Aquaman1234555@users.noreply.github.com>
2022-03-12 22:44:57 +0530

    Add files via upload

---
+++ scratch_canvas (1).py
@@ -0,0 +1,15 @@
+import turtle
+turtle.pencolor('blue')
+turtle.penup()
+turtle.setposition(-100, 100)
+turtle.pendown()
+turtle.pencolor()
+#write the color of pen in inverted commas
+turtle.circle(85)
+turtle.penup()
+turtle.goto(-200, -200)
+#write the location to fill the gap
+turtle.pendown()
+turtle.pencolor()
+#write the color of pen in inverted commas
+turtle.circle(200)
